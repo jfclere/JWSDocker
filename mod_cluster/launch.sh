2b7fd7911b97c47e86422b781299f29f53efc174
Jean-Frederic Clere <jfclere@gmail.com>
2014-10-20 17:01:14 +0200

    Add logic to allow only the other docker container to fill the tables.

--- mod_cluster/launch.sh
+++ mod_cluster/launch.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+NETIP=`ifconfig eth0 | grep "inet " | awk '{ print $2 }' | awk 'BEGIN{FS="."}{print $1 "." $2 "." $3}'`
+RUN sed -i "s|127.0.0.1|${NETIP}|" $EWS_HOME/httpd/conf.d/mod_cluster.conf
+
 exec $EWS_HOME/httpd/sbin/apachectl -DFOREGROUND
