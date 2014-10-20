9b1c9a6e19437619a3b381de5a376a24e7c317ec
Jean-Frederic Clere <jfclere@gmail.com>
2014-10-20 18:12:23 +0200

    Configure mod_cluster in tomcat7.

--- tomcat7/launch.sh
+++ tomcat7/launch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sed -i "/SHUTDOWN/a <Listener className=\"org.jboss.modcluster.container.catalina.standalone.ModClusterListener\" ProxyList=\"${WEB_PORT_6666_TCP_ADDR}:${WEB_PORT_6666_TCP_PORT}\"/>" /opt/jboss/ews/tomcat7/conf/server.xml
 /opt/jboss/ews/tomcat7/bin/startup.sh
