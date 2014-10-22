97eaad9e72cea9f7a40bb14f115c841c3e64c167
root <root@localhost.localdomain>
2014-10-22 14:29:25 +0200

    tail logs/catalina.out...

--- tomcat7/launch.sh
+++ tomcat7/launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sed -i "/SHUTDOWN/a <Listener className=\"org.jboss.modcluster.container.catalina.standalone.ModClusterListener\" ProxyList=\"${WEB_PORT_6666_TCP_ADDR}:${WEB_PORT_6666_TCP_PORT}\"/>" /opt/jboss/ews/tomcat7/conf/server.xml
 /opt/jboss/ews/tomcat7/bin/startup.sh
+tail -f /opt/jboss/ews/tomcat7/logs/catalina.out
